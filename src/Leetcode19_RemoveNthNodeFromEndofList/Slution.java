3562e54a2f641ce34943e453e150460ab9514628
Lekang <lekangdu@gmail.com>
2019-09-17 20:17:05 -0400

    19 1st time

---
+++ src/Leetcode19_RemoveNthNodeFromEndofList/Slution.java
@@ -0,0 +1,28 @@
+package Leetcode19_RemoveNthNodeFromEndofList;
+
+import Singly_LinkedList.ListNode;
+
+public class Slution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode last = dummy;
+        if (dummy.next == null) {
+            return dummy.next;
+        }
+        // 1. move the pre to the Nth node
+        for (int i = 1; i <= n + 1; i++) {
+            pre = pre.next;
+        }
+        // 2, move pre and last at same time until pre get to last node
+        while (pre != null) {
+            pre = pre.next;
+            last = last.next;
+        }
+        // 3. remove last.next node
+        last.next = last.next.next;
+
+        return dummy.next;
+    }
+}
---
+++ src/Singly_LinkedList/ListNode.java
@@ -0,0 +1,15 @@
+package Singly_LinkedList;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+ public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+ }
