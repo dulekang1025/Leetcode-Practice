b4db697f9c2f1b784b8385846cd27d4d0bcd55e7
Lekang <lekangdu@gmail.com>
2019-09-26 22:56:42 -0400

    Q2

---
+++ src/Leetcode_2_Add_Two_Numbers/Solution.java
@@ -0,0 +1,31 @@
+package Leetcode_2_Add_Two_Numbers;
+
+import Singly_LinkedList.ListNode;
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode p1 = l1, p2 = l2;
+        ListNode result = new ListNode(0);
+        ListNode cur = result;
+        int sum = 0;
+        while(p1 != null || p2 != null){
+            if(p1 != null){
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if(p2 != null){
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            cur.next = new ListNode(sum % 10);
+            sum /= 10;
+            cur = cur.next;
+
+        }
+        if(sum == 1){
+            cur.next = new ListNode(1);
+        }
+        return result.next;
+
+    }
+}
