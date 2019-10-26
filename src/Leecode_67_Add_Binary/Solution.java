616b796e2e8e97f5484b1a13e92aeea0f6f8a1b6
Lekang <lekangdu@gmail.com>
2019-10-26 08:32:51 -0400

    ~

--- src/Leecode_67_Add_Binary/Solution.java
+++ src/Leecode_67_Add_Binary/Solution.java
@@ -3,29 +3,21 @@ package Leecode_67_Add_Binary;
 public class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb =  new StringBuilder();
-
         int i = a.length() - 1;
         int j = b.length() - 1;
-
         int head = 0;
-
         while(i >= 0 || j >=0){
             int sum = head;
-
             if(i >= 0) sum += a.charAt(i) - '0';
             if(j >= 0) sum += b.charAt(j) - '0';
-
             sb.append(sum % 2);
             head = sum / 2;
             j--;
             i--;
-
         }
         if(head == 1) {
             sb.append(head);
         }
-
         return sb.reverse().toString();
-
     }
 }
