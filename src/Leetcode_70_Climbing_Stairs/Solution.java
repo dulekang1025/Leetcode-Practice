1d11f7426158d9856e47e98a6baee30a46905dc6
Lekang <lekangdu@gmail.com>
2019-10-31 22:22:28 -0400

    Q70

---
+++ src/Leetcode_70_Climbing_Stairs/Solution.java
@@ -0,0 +1,20 @@
+package Leetcode_70_Climbing_Stairs;
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] num = new int[n];
+        if(n == 1){
+            return 1;
+        }else if(n == 2){
+            return 2;
+        }else{
+            num[0] = 1;
+            num[1] = 2;
+            for(int i = 2; i < n; i++){
+                num[i] = num[i-1] + num[i-2];
+            }
+            return num[n-1];
+        }
+
+    }
+}
