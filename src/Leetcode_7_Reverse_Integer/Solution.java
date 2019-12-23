687b9276b5631c4391087675c1a9261428b3472e
Lekang <lekangdu@gmail.com>
2019-12-22 20:56:39 -0500

    Q7

---
+++ src/Leetcode_7_Reverse_Integer/Solution.java
@@ -0,0 +1,13 @@
+package Leetcode_7_Reverse_Integer;
+
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        while(x != 0){
+            if(Math.abs(res) > Integer.MAX_VALUE / 10) return 0;  // ？？？？？？？？？？？？
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        return res;
+    }
+}
