4d690e6beec678393ed965dcb4e3f124ad8c1fc1
Lekang <lekangdu@gmail.com>
2019-12-26 10:48:44 -0500

    Q167

---
+++ src/Leetcode_167_Two_Sum_II_Input_array_is_sorted/Solution.java
@@ -0,0 +1,17 @@
+package Leetcode_167_Two_Sum_II_Input_array_is_sorted;
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                if(numbers[i] + numbers[j] == target){
+                    res[0] = i + 1;
+                    res[1] = j + 1;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
