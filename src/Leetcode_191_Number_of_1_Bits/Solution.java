233fae31d2bc59caf6ecde9ad09844ef355d805a
Lekang <lekangdu@gmail.com>
2019-12-27 20:11:04 -0500

    Q191

---
+++ src/Leetcode_191_Number_of_1_Bits/Solution.java
@@ -0,0 +1,13 @@
+package Leetcode_191_Number_of_1_Bits;
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res = 0;
+        while(n != 0){
+            if((n&1) == 1) res++;
+            n = n >>> 1;
+        }
+        return res;
+    }
+}
