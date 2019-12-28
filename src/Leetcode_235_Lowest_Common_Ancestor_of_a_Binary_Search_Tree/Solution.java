7af3ee15b9e0807a09590730833693410aec624d
Lekang <lekangdu@gmail.com>
2019-12-28 16:42:00 -0500

    Q235

---
+++ src/Leetcode_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
@@ -0,0 +1,24 @@
+package Leetcode_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
+
+import TreeNode.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        TreeNode ans = null;
+        if(p.val == root.val || q.val == root.val) return root;
+        if(p.val < root.val && q.val < root.val) ans = lowestCommonAncestor(root.left,p,q);
+        if(p.val > root.val && q.val > root.val) ans = lowestCommonAncestor(root.right,p,q);
+        if((p.val > root.val && q.val < root.val) || (p.val < root.val && q.val > root.val)) return root;
+        return ans;
+    }
+}
