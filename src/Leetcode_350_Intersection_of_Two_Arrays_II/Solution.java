e4435a2b5c0904a8d97971b6ffe7b2d16af43666
Lekang <lekangdu@gmail.com>
2020-03-24 22:43:15 -0400

    Q350

---
+++ src/Leetcode_350_Intersection_of_Two_Arrays_II/Solution.java
@@ -0,0 +1,31 @@
+package Leetcode_350_Intersection_of_Two_Arrays_II;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] == nums2[j]){
+                ans.add(nums1[i]);
+                i++;
+                j++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }else{
+                i++;
+            }
+        }
+        int[] res = new int[ans.size()];
+        for(int k = 0; k < ans.size(); k++){
+            res[k] = ans.get(k);
+        }
+        return res;
+    }
+}
