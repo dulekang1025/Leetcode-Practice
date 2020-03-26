673e7b470d290cb5ba29492a71c4c5a30fa016f8
Lekang <lekangdu@gmail.com>
2020-03-26 19:56:08 -0400

    Q258

---
+++ src/Leetcode_258_Add_Digits/Solution.java
@@ -0,0 +1,15 @@
+package Leetcode_258_Add_Digits;
+
+class Solution {
+    public int addDigits(int num) {
+        while(num / 10 > 0){
+            int sum = 0;
+            while(num > 0){
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
