fc4bf68dd3456e3bee5e652e455072300243d951
Lekang <lekangdu@gmail.com>
2020-03-26 20:11:44 -0400

    Q283

---
+++ src/Leetcode_283_Move_Zeroes/Solution.java
@@ -0,0 +1,16 @@
+package Leetcode_283_Move_Zeroes;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0) {
+                nums[count++] = nums[i];
+            }
+        }
+        System.out.println(count);
+        for(int i = count; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
