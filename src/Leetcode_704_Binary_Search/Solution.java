8be5843acc43efecfef4103fdcda2df28c3dd33b
Lekang <lekangdu@gmail.com>
2020-04-27 16:55:43 -0400

    Q704

---
+++ src/Leetcode_704_Binary_Search/Solution.java
@@ -0,0 +1,18 @@
+package Leetcode_704_Binary_Search;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            if(nums[mid] > target){
+                high = mid - 1;
+            }else if(nums[mid] < target){
+                low = mid + 1;
+            }else return mid;
+        }
+        return -1;
+    }
+}
