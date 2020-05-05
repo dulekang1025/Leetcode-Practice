4d5ffc6d5785bc686a08d909c345dc7ad0ffb61c
Lekang <lekangdu@gmail.com>
2020-05-04 20:08:59 -0400

    Q8

---
+++ src/Leetcode_8_String_to_Integer/Solution.java
@@ -0,0 +1,43 @@
+package Leetcode_8_String_to_Integer;
+
+class Solution {
+    public int myAtoi(String str) {
+
+
+        // 2. whitespaces
+        str = str.trim();
+
+        // 1. null or empty string
+        if (str == null || str.length() == 0) return 0;
+
+        // 3. +/- sign
+        boolean positive = true;
+        int i = 0;
+        if (str.charAt(0) == '+') {
+            i++;
+        } else if (str.charAt(0) == '-') {
+            positive = false;
+            i++;
+        }
+
+        // 4. calculate real value
+        double tmp = 0;
+        for ( ; i < str.length(); i++) {
+            int digit = str.charAt(i) - '0';
+
+            if (digit < 0 || digit > 9) break;
+
+            // 5. handle min & max
+            if (positive) {
+                tmp = 10*tmp + digit;
+                if (tmp > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            } else {
+                tmp = 10*tmp - digit;
+                if (tmp < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            }
+        }
+
+        int ret = (int)tmp;
+        return ret;
+    }
+}
