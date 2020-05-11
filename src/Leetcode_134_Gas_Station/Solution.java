21709791f676fa1451597318c2d20f335ae4b96b
Lekang <lekangdu@gmail.com>
2020-05-10 20:05:46 -0400

    Q134

---
+++ src/Leetcode_134_Gas_Station/Solution.java
@@ -0,0 +1,19 @@
+package Leetcode_134_Gas_Station;
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0;
+        int sum = 0;
+        int start = 0;
+        for(int i = 0; i < gas.length; i++){
+            total += gas[i] - cost[i];
+            if(sum < 0){
+                sum = gas[i] - cost[i];
+                start = i;
+            }else{
+                sum += gas[i] - cost[i];
+            }
+        }
+        return total >= 0 ? start : -1;
+    }
+}
