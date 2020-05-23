d7ec2dfbab9e64fec90eb2792eb193a1a617ed0c
Lekang <lekangdu@gmail.com>
2020-05-23 08:50:23 -0400

    Q986

---
+++ src/Leetode_986_Interval_List_Intersections/Solution.java
@@ -0,0 +1,25 @@
+package Leetode_986_Interval_List_Intersections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int []> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while(i < A.length && j < B.length){
+            int start = Math.max(A[i][0], B[j][0]);
+            int end = Math.min(A[i][1], B[j][1]);
+            if(start <= end){
+                ans.add(new int[]{start, end});
+            }
+            if(A[i][1] > B[j][1]){
+                j++;
+            }else{
+                i++;
+            }
+        }
+        return ans.toArray(new int[ans.size()][2]);
+    }
+}
