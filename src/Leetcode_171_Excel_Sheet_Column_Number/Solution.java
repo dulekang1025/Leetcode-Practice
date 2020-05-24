8b3a83da45189565434d72beff85e1db60f1ac3e
Lekang <lekangdu@gmail.com>
2020-05-24 15:33:25 -0400

    Q171

---
+++ src/Leetcode_171_Excel_Sheet_Column_Number/Solution.java
@@ -0,0 +1,11 @@
+package Leetcode_171_Excel_Sheet_Column_Number;
+
+class Solution {
+    public int titleToNumber(String s) {
+        int res = 0;
+        for(char c : s.toCharArray()){
+            res = res * 26 + (c - 'A' + 1);
+        }
+        return res;
+    }
+}
