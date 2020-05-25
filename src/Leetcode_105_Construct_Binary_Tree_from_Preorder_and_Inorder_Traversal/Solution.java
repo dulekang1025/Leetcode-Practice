a802bc47f4248c8354a65e3156659946b7a081f0
Lekang <lekangdu@gmail.com>
2020-05-25 15:56:02 -0400

    ~

--- src/Leetcode_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.java
+++ src/Leetcode_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.java
@@ -10,6 +10,16 @@ import TreeNode.TreeNode;
 //不妨设为l，我们可以知道preorder从1到l+1就是根节点左子树的前序遍历，
 //剩下的最后一部分就是根节点右子树的前序遍历。
 
+//本题在属于二叉树遍历的经典题目，已知二叉树的两个遍历序列构造二叉树，有如下性质：
+//
+//若已知先序和中序，则可以构造出唯一的二叉树
+//
+//若已知先序和后序，则可以构造出多颗不同的二叉树
+//
+//若已知中序和后序，则可以构造出唯一的二叉树
+//
+//本题中我们已知的条件为中序遍历和后序遍历，所以我们一定可以构造出唯一的二叉树。
+
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return helper(preorder,inorder,0,0,inorder.length - 1);
