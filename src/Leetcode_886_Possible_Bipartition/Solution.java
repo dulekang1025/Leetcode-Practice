7b0d7c314900939bbc483a7e910506e36643a799
Lekang <lekangdu@gmail.com>
2020-05-25 22:00:36 -0400

    Q886

---
+++ src/Leetcode_886_Possible_Bipartition/Solution.java
@@ -0,0 +1,39 @@
+package Leetcode_886_Possible_Bipartition;
+
+import java.util.*;
+
+class Solution {
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        if(dislikes.length == 0) return true;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int[] e : dislikes){
+            map.putIfAbsent(e[0], new ArrayList<>());
+            map.putIfAbsent(e[1], new ArrayList<>());
+            map.get(e[0]).add(e[1]);
+            map.get(e[1]).add(e[0]);
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        int[] visited = new int[N + 1];
+
+        for(int i = 1; i <= N; i++){
+            if(visited[i] != 0 || !map.containsKey(i)) continue;
+            queue.add(i);
+            visited[i] = 1;
+            while(!queue.isEmpty()){
+                int cur = queue.poll();
+                //System.out.println(cur);
+                int neiLabel = visited[cur] == 1 ? 2 : 1;
+                for(int nei : map.get(cur)){
+                    if(visited[nei] == 0){
+                        visited[nei] = neiLabel;
+                        queue.add(nei);
+                    }else{
+                        if(visited[nei] != neiLabel)
+                            return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
