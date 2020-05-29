2de86be242275c641d1dd1e2d90260f7368fcc51
Lekang <lekangdu@gmail.com>
2020-05-28 21:55:35 -0400

    Q300

---
+++ src/Leetcode_300_Longest_Increasing_Subsequence/Solution.java
@@ -0,0 +1,22 @@
+package Leetcode_300_Longest_Increasing_Subsequence;
+
+import java.util.Arrays;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < dp.length; i++){
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}
