976272ddce5baff8f034e4106f6103f1d8865aa9
Lekang <lekangdu@gmail.com>
2020-06-05 16:37:48 -0400

    BubbleSort

--- src/Sorting_Algorithm/BubbleSort.java
+++ src/Sorting_Algorithm/BubbleSort.java
@@ -1,4 +1,16 @@
 package Sorting_Algorithm;
 
 public class BubbleSort {
+    public void bubbleSort(int[] a){
+        int len = a.length;
+        for(int i = 0; i < len - 1; i++){
+            for(int j = 0; j < len - i - 1; j++){
+                if(a[j] > a[j + 1]){
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = a[j];
+                }
+            }
+        }
+    }
 }
