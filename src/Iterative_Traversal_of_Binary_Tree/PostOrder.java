8e15a61b852febe54978e45d82a9c9fb961a496c
Lekang <lekangdu@gmail.com>
2020-06-07 14:47:02 -0400

    Update PostOrder.java

--- src/Iterative_Traversal_of_Binary_Tree/PostOrder.java
+++ src/Iterative_Traversal_of_Binary_Tree/PostOrder.java
@@ -1,4 +1,28 @@
 package Iterative_Traversal_of_Binary_Tree;
 
+import TreeNode.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class PostOrder {
+
+    public void postOrder(TreeNode root){
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode lastVisited = root;
+        while(!stack.isEmpty() || root != null){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.peek();
+            if(root.right == null || root.right == lastVisited){
+                System.out.println(root.val);
+                lastVisited = stack.pop();
+                root = null;
+            }else {
+                root = root.right;
+            }
+        }
+    }
 }
