0f0c846cee29f253c6a2afcbd9b631e223054560
Lekang <lekangdu@gmail.com>
2020-06-16 15:43:50 -0400

    Q151

---
+++ src/Leetcode_151_Reverse_Words_in_a_String/Solution.java
@@ -0,0 +1,18 @@
+package Leetcode_151_Reverse_Words_in_a_String;
+
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] arr = s.trim().split(" ", -1);
+        for(int i = arr.length - 1; i >= 0; i--){
+            System.out.println(arr[i]);
+            if(!arr[i].equals("") || !arr[i].equals("")){
+                sb.append(arr[i].trim());
+                if(i != 0){
+                    sb.append(" ");
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
