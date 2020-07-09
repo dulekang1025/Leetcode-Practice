876571b717492cc9cb5fc7cf99fee68c67ab7960
Lekang <lekangdu@gmail.com>
2020-07-09 10:51:25 -0400

    Q232

---
+++ src/Leetcode_232_Implement_Queue_using_Stacks/MyQueue.java
@@ -0,0 +1,55 @@
+package Leetcode_232_Implement_Queue_using_Stacks;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class MyQueue {
+
+    Deque<Integer> inStack;
+    Deque<Integer> outStack;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        inStack = new ArrayDeque<>();
+        outStack = new ArrayDeque<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(outStack.isEmpty()){
+            while(!inStack.isEmpty()){
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if(outStack.isEmpty()){
+            while(!inStack.isEmpty()){
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return outStack.isEmpty() && inStack.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
