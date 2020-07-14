beb8860ead434b20648a59593f1959532d2c8a43
Lekang <lekangdu@gmail.com>
2020-07-13 22:03:54 -0400

    Q1138

---
+++ src/Leetcode_1138_Alphabet_Board_Path/Solution.java
@@ -0,0 +1,31 @@
+package Leetcode_1138_Alphabet_Board_Path;
+
+class Solution {
+    public String alphabetBoardPath(String target) {
+        int i = 0, j = 0;
+        StringBuilder sb = new StringBuilder();
+        for(char c : target.toCharArray()){
+            int val = c - 'a';
+            int row = val / 5;
+            int col = val % 5;
+            while(j > col){
+                j--;
+                sb.append('L');
+            }
+            while(i > row){
+                i--;
+                sb.append('U');
+            }
+            while(j < col){
+                j++;
+                sb.append('R');
+            }
+            while(i < row){
+                i++;
+                sb.append('D');
+            }
+            sb.append('!');
+        }
+        return sb.toString();
+    }
+}
