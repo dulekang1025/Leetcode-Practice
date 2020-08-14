9e6b0243aafe4b5beefc4c1199a8dc4ed9c355a1
Lekang <lekangdu@gmail.com>
2020-08-14 11:22:10 -0400

    Q409

---
+++ src/Leetcode_409_Longest_Palindrome/Solution.java
@@ -0,0 +1,22 @@
+package Leetcode_409_Longest_Palindrome;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int ans = 0;
+        for(char k : map.keySet()){
+            int v = map.get(k);
+            ans += v / 2 * 2;
+            if(v % 2 == 1 && ans % 2 == 0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
