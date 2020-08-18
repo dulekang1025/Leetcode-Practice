16803f0c2530d6bbd31b13d601a0e73ec653bcb2
Lekang <lekangdu@gmail.com>
2020-08-18 14:20:51 -0400

    Q13 2

---
+++ src/Leetcode_13_Roman_to_Integer/Solution2.java
@@ -0,0 +1,26 @@
+package Leetcode_13_Roman_to_Integer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution2 {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int res = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))){
+                res -= map.get(s.charAt(i));
+            }else{
+                res += map.get(s.charAt(i));
+            }
+        }
+        return res;
+    }
+}
