0e9a34a0ea17e41dd843ab5faeaf6b622781fb78
Lekang <lekangdu@gmail.com>
2020-08-28 09:04:52 -0400

    Q470

---
+++ src/Leetcode_470_Implement_Rand10_Using_Rand7/Solution.java
@@ -0,0 +1,10 @@
+package Leetcode_470_Implement_Rand10_Using_Rand7;
+
+class Solution extends SolBase {
+    public int rand10() {
+        while(true){
+            int num = (rand7() - 1) * 7 + rand7();
+            if(num <= 40) return num % 10 + 1;
+        }
+    }
+}
