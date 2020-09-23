0bf42286e8f6b3b41b97cb1cd1589f91ac559bb1
Lekang <lekangdu@gmail.com>
2020-09-23 09:30:03 -0400

    Q252

---
+++ src/Leetcode_252_Meeting_Rooms/Solution.java
@@ -0,0 +1,17 @@
+package Leetcode_252_Meeting_Rooms;
+
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int[] prev = null;
+        for(int i = 0; i < intervals.length; i++){
+            if(prev != null){
+                if(prev[1] > intervals[i][0]){
+                    return false;
+                }
+            }
+            prev = intervals[i];
+        }
+        return true;
+    }
+}
