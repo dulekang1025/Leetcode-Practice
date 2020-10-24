fc5f5d256699623eb3236cb09aa808c2b445de79
Lekang <lekangdu@gmail.com>
2020-10-24 16:09:00 -0400

    Update Solution2.java

--- src/Leetcode_605_Can_Place_Flowers/Solution2.java
+++ src/Leetcode_605_Can_Place_Flowers/Solution2.java
@@ -1,6 +1,8 @@
 package Leetcode_605_Can_Place_Flowers;
 
 public class Solution2 {
+    // 防御式编程思想：在 flowerbed 数组两端各增加一个 0，
+    // 这样处理的好处在于不用考虑边界条件，任意位置处只要连续出现三个 0 就可以栽上一棵花。
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int[] arr = new int[flowerbed.length + 2];
         int count = 0;
